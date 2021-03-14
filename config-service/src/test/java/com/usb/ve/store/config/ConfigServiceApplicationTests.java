ecbc394ba4ace2a15141dd3a9d5d107a89c2819f
eowkin <eowkin@gmail.com>
2021-03-14 17:56:23 -0400

    se agrego la configuracion central de los microservicios config-service

---
+++ config-service/src/test/java/com/usb/ve/store/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usb.ve.store.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
